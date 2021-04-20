041cf688d82354bc5f616d71a280800541023480
MarkusSandberg <markus.sandberg@student.tabyenskilda.se>
2021-04-20 12:11:42 +0200

    Problem med "Rows"

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
+/* Toggle between adding and removing the "responsive" class to nav when the user clicks on the icon */
+function ChangeResponsive() {
+  var x = document.getElementById("navID");
+  if (x.className === "nav") {
     x.className += " responsive";
   } else {
-    x.className = "topnav";
+    x.className = "nav";
   }
-} 
+}
